7332d24d20b1f8ebbef2f53648b4fbfd4f8e9b26
Max Liu <zl2480@columbia.edu>
2018-07-16 22:15:42 -0400

    Update data_related.py

--- data_related.py
+++ data_related.py
@@ -1,1 +1,16 @@
+# Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+      pass
+    
+# use vmin and vmax to normalize the color
+ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
+    
+# print time usage with better formatting
+from datetime import timedelta
+str(timedelta(seconds=5))  # 00:00:05
+
+
 
